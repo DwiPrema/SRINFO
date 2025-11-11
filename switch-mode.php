d1f7857a7c417685662d2fb6e373c1fe60b79dab
DwiPremayasa <madedwiprema08@gmail.com>
2025-11-11 13:20:52 +0800

    Add files via upload

---
+++ switch-mode.php
@@ -0,0 +1,11 @@
+<div class="toggle-mode">
+    <button class="btn-mode">
+        <div class="img-container">
+            <img src="assets/moon-icon.png" alt="" id="img-mode">
+        </div>
+
+        <div class="text" id="text-mode">
+            <p>dark</p>
+        </div>
+    </button>
+</div>
